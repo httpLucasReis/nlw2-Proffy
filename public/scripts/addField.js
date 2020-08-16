d0c9ca6f305519f9021a9e6035a514e2a0ca022e
httpLucasReis <manoel.reisiema@gmail.com>
2020-08-16 16:56:54 -0300

    Add addting the delete schedule button

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -1,3 +1,6 @@
+// schedules salvos 
+let schedulesSaved = [];
+
 // Procura botão
 document.querySelector("#add-time").addEventListener('click', cloneField);
 
@@ -8,16 +11,49 @@ function cloneField() {
     // cloneNode -> copia até mesmo os filhos
     const newFieldsContainer = document.querySelector(".schedule-item").cloneNode(true);
 
-    //limpar os campos 
+    console.log(newFieldsContainer);
 
+    // Pegar o botão delete
+    const deleteButton = newFieldsContainer.querySelector(".delete-button")
+
+    // o total de botões adicionados -> o novo index de um schedule é igual a total de schedules adicionados
+    const totalSchedules = document.querySelectorAll("#schedule-items .schedule-item+.schedule-item").length;
+
+    // Limpando schedules salvos -> caso o usuário tenha apagado todos os schedules
+    if( totalSchedules == 0){
+        schedulesSaved = schedulesSaved.slice(0,schedulesSaved-1);
+    }
+    
+    // Adicionando a função remove 
+    deleteButton.setAttribute('onclick','removeItem('+(totalSchedules)+')');
+
+    // Torna-lo visível 
+    deleteButton.classList.toggle("active");
+
+    //limpar os campos 
     const fields = newFieldsContainer.querySelectorAll("input");
 
     // Limpando cada field dos inputs
-    fields.forEach(field => field.value = '');
+    fields.forEach((field) => {
+        field.value = '';
+    });
 
     // Colocar na página
     document.querySelector("#schedule-items").appendChild(newFieldsContainer);
+
+    schedulesSaved.push(newFieldsContainer);
 }
 
+function removeItem(index){
+    "use strict";
+    // Pegando todos os index
+     const schedule = schedulesSaved[index]
+
+     console.log(schedule)
+
+    // Removendo o schedule que corresponde ao index passado como parâmetro
+    schedule.parentNode.removeChild(schedule);
+
+}
 
  
